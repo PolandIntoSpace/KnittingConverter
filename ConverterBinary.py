22e5bb73955076d4d5e8fdd432442b26ce97b406
PolandIntoSpace <you@example.com>
2019-08-18 00:32:35 +0200

    Made a few changes regarding the reading and saving the file. - User can now input the (input)file path. If loading fails the programm reports the message and stops. - The output is being saved in the same way the input looks like, in the file "output.txt". If saving fails an error message will be caught and the program ends.
    TODO:
    - save output in same directory where input comes from?
    - let's see how it works with pictures

--- ConverterBinary.py
+++ ConverterBinary.py
@@ -1,5 +1,7 @@
+import sys
 import numpy as np
 
+# input is np.array with bools
 def printpattern(patternBoolean):
     print("function printpattern")
     for row in patternBoolean:
@@ -10,44 +12,50 @@ def printpattern(patternBoolean):
             else:
                 row_tmp.append('0')
         print(row_tmp)
-    #input is np.array with bools
 
 
+print("[START - ConvertBinary()]")
+#inputFile = "input.txt"
+print("[LOAD] Selecting input file")
+inputFile = input("\tPlease enter the filepath (only name is enough if it's in the same folder): ")
+#print("\tYour selected file is: " + inputFile)
 
-#test for commit
-print("Hello World!")
-I = np.eye(3)
-print(I)
+try:
+    # read file into matrix form
+    data = np.loadtxt(inputFile, dtype=bool, delimiter=' ')
+except Exception as e:
+    print("\t[ERROR] Loading failed! Following exception occured: \n\t {:s} \n[END]".format(str(e)))
+    sys.exit(1)
 
-inputFile = "input.txt"
 
-# read file into matrix form
-data = np.loadtxt(inputFile, dtype=bool, delimiter=' ')
-
-print('[LOADING] The uploaded input from file {:s} is:'.format(inputFile))
-print(data)
 print("\n[INVERTING] The even lines are being inverted.")
 # invert all even rows (python is 0-index based, so for us all uneven rows)
 # range(start, end, step)
 nbrRows = len(data)
 nbrCols = len(data[0])
-dataConv = data;
+dataConv = data
+
 for row in range(nbrRows-2, 0, -2):
-    print(row)
-    #print(data[row])
     for pix in range(0, nbrCols):
         dataConv[row][pix] = not dataConv[row][pix]
-        #print("Old pixel: {:d}, and new pixel on this spot {:d}".format(data[row][pix],dataConv[row][pix]))
 
 # last line of the sketch is first line in the code due to how the txt file is read.
-if nbrRows%2 == 0:  # if even nbr of all rows invert first(python)/last(sketch) line
+if nbrRows % 2 == 0:  # if even nbr of all rows invert first(python)/last(sketch) line
     for pix in range(0, nbrCols):
         dataConv[0][pix] = not dataConv[0][pix]
 
-print(dataConv)
-print("\n")
-printpattern(dataConv)
+#printpattern(dataConv)
 
 print("[FLIP] Flip pattern horizontally")
 dataConv = np.fliplr(dataConv)
-printpattern(dataConv)
+#printpattern(dataConv)
+
+# Save file with new pattern
+print("[SAVE] Save new pattern as \"output.txt\"")
+try:
+    np.savetxt("output.txt", dataConv, fmt="%i")
+except Exception as e:
+    print("\t[ERROR] Saving failed! Following exception occured: \n\t %s\n[END]".format(str(e)))
+    sys.exit(1)
+
+print("[END - successfully]")
